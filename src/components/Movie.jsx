6a7128a98326702ea662184c43d7823f80824ffe
kimnaewoo <140692045+kimnaewoo@users.noreply.github.com>
2024-07-08 20:01:37 +0900

    movie project setup

---
+++ src/components/Movie.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Movie(title, poster_path, vote_average) {
+  return (
+    <div className="movie-container">
+      <img src="" alt="" />
+      <div className="movie-info">
+        <h4></h4>
+        <span></span>
+      </div>
+    </div>
+  );
+}
